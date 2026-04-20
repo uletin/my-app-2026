af3b1b7019956d63d3b0540f1312b5f67669520d
Uletin <uletin@Uletin-Mac.local>
2026-04-20 11:58:11 +0800

    first commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,3 @@
+export default function LearnPage() {
+  return <p>test</p>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Hello Al Fathir
           </a>
         </div>
       </main>

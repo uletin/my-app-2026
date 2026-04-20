4218f7fa9761ba81ded50068c7ab66fa8b08d9fa
Uletin <uletin@Uletin-Mac.local>
2026-04-20 12:01:44 +0800

    feat: new learn page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,14 @@
 export default function LearnPage() {
-  return <p>test</p>;
+  return (
+    <div>
+      <div>
+        <img src="/palette.svg" alt="Learn Image" className="w-full h-auto" />
+        <p>Design</p>
+      </div>
+      <div className="p-8">
+        <p>Figma</p>
+        <p>8 Hours Ago</p>
+      </div>
+    </div>
+  );
 }
